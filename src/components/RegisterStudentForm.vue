96e4afa1f456c56c697a8fd8425635461966e3db
Unknown <jorge.plaza@usach.cl>
2019-09-16 11:18:04 -0300

    Change get careers endpoint

--- src/components/RegisterStudentForm.vue
+++ src/components/RegisterStudentForm.vue
@@ -71,7 +71,7 @@
 
       async showCareers(){
     
-          await axios.get('http://192.241.158.156:8081/careers')
+          await axios.get('http://192.241.158.156:8081/career/getCareers')
           .then(res=>{
             this.careers = res.data.map(item => item.name)
               console.log(res.data)

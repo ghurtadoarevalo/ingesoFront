7a87cb9be26b26e1fcc6716a284cd3df0a25022f
mcoronado <matias.coronado@usach.cl>
2019-09-21 20:45:18 -0300

    LF v7

---
+++ test/Test Suites/PruebaTestSuites.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>PruebaTestSuites</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>edcb5cd3-3431-4805-8cdc-91dbb0cfcb13</testSuiteGuid>
+</TestSuiteEntity>

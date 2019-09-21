710fcf193f3939ded4f5aa052821f52ac9650981
mcoronado <matias.coronado@usach.cl>
2019-09-21 20:46:24 -0300

    v8

--- test/Test Suites/PruebaTestSuites.ts
+++ test/Test Suites/PruebaTestSuites.ts
@@ -10,4 +10,22 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>edcb5cd3-3431-4805-8cdc-91dbb0cfcb13</testSuiteGuid>
+   <testCaseLink>
+      <guid>79a60bfc-607a-4bed-b775-66ab405faf5e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestA</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>76bec6e0-c37f-4085-aee3-aa42be7f49bd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestB</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ececf745-8ca3-43e1-98bf-b53ced3ea648</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestC</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>

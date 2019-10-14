875e4c9f3b0e1ad28e66989259d02052ec13c4fe
Unknown <jorge.plaza@usach.cl>
2019-10-13 23:17:47 -0300

    Test Jenkins

--- src/components/ReservationForm.vue
+++ src/components/ReservationForm.vue
@@ -215,6 +215,7 @@
 
             checkNumber(number) {
                 var nm = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+                
                 if(nm.test(number)) {
                     this.validator = false;
                 }
